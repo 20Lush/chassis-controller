eebf4d338a7b16d9980bd42169aaa3b41175980b
Zachary Johnson <zachfsx83@gmail.com>
2024-04-21 11:37:33 -0700

    [OpenSauce] rework pwm input

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,24 @@
 #include <Adafruit_NeoPixel.h>
 #include <Servo.h>
+#include <sstream>
+#include <string>
+
+#include "r8ts.hpp"
 
 Adafruit_NeoPixel pixel(1, 25, NEO_GRB + NEO_KHZ800);
 
-
-const int channel_1 = 5;
+R8TS r8ts;
+std::array<int, R8TS::num_channels> inputs;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(channel_1, INPUT_PULLUP);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
-  pixel.fill(0xFF0000);
-  pixel.show();
-  int val = pulseIn(channel_1, HIGH, 25000);
-  Serial.println(val);
-	delay(500);
 
-  pixel.fill(0x000FF0);
-  pixel.show();
-
-  delay(500);
+  inputs = r8ts.getPercents();
 
 }
