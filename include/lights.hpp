c93b1129130a47e8644c7cf464cab9b3606536a0
Zachary Johnson <zachfsx83@gmail.com>
2024-06-05 15:17:14 -0700

    i rm rf'd this accidentally

---
+++ include/lights.hpp
@@ -0,0 +1,9 @@
+#include "Adafruit_NeoPixel.h"
+
+void colorWipe(Adafruit_NeoPixel& strip, uint32_t color, int wait) {
+  for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
+    strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
+    strip.show();                          //  Update strip to match
+    delay(wait);                           //  Pause for a moment
+  }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,19 @@
 
 #include "motor.hpp"
 #include "R8EF.hpp"
+#include "lights.hpp"
 
 Adafruit_NeoPixel pixel(1, 25, NEO_GRB + NEO_KHZ800);
+
+const size_t PCB_LIGHTS_BRIGHTNESS = 50;
+Adafruit_NeoPixel pcb_lights(1, 21, NEO_GRB + NEO_KHZ800);
 
 R8EF rx;
 EIGHT_CHANNEL raw_inputs;
 
-// Left, Right, Back
-const unsigned int NUM_MOTORS = 3;
-std::array<Motor, NUM_MOTORS> motors = {Motor(A2), Motor(A1), Motor(A3)};
+// Left, Right
+const unsigned int NUM_MOTORS = 2;
+std::array<Motor, NUM_MOTORS> motors = {Motor(A2), Motor(A1)};
 
 const double sqrt3div2 = sqrt(3)/2;
 
@@ -27,25 +31,33 @@ double fmap(double x, double in_min, double in_max, double out_min, double out_m
 //////////////////////////////////////////////////////////////
 void setup() {
   Serial.begin();
-  
+  rx.Init();
+
   for(Motor& motor : motors) {
-
     motor.Init();
-
   }
 
   delay(1500);
-
 }
 
 void loop() {
-  double X = fmap(raw_inputs.ch4, 1000, 2000, -1, 1);
-  double Y = fmap(raw_inputs.ch3, 1000, 2000, -1, 1);
-  double R = fmap(raw_inputs.ch1, 1000, 2000, -1, 1);
 
-  motors[0].Set_uSec((((-0.5 * X) - (sqrt3div2 * Y) - R) * 500) + 1500);
-  motors[1].Set_uSec((((-0.5 * X) + (sqrt3div2 * Y) - R) * 500) + 1500);
-  motors[2].Set_uSec((( X - R) * 500) + 1500);
+  raw_inputs = rx.getRaw();
+
+  // example mapping the raw inputs from the rxer to float percentage
+  // double X1 = fmap(raw_inputs.ch4, 1000, 2000, -1, 1);
+  // double Y1 = fmap(raw_inputs.ch3, 1000, 2000, -1, 1);
+  // double X2 = fmap(raw_inputs.ch1, 1000, 2000, -1, 1);
+  // double Y2 = fmap(raw_inputs.ch2, 1000, 2000, -1, 1);
+
+  // these channel assignments might be wrong idk
+  uint16_t X1 = raw_inputs.ch4;
+  uint16_t X2 = raw_inputs.ch1;
+
+  // motor setter routine. 1000-2000, so 1500 corresponds to zero.
+  motors[0].Set_uSec(X1);
+  motors[1].Set_uSec(X2);
+
 
 }
 
@@ -54,12 +66,15 @@ void loop() {
 //////////////////////////////////////////////////////////////
 void setup1() {
 
-  rx.Init();
-
+  pcb_lights.begin();
+  pcb_lights.show();
+  pcb_lights.setBrightness(PCB_LIGHTS_BRIGHTNESS);
 }
 
 void loop1() {
 
-  raw_inputs = rx.getRaw();
-
+  
+  colorWipe(pcb_lights, pcb_lights.Color(255,   0,   0)     , 50); // Red
+  colorWipe(pcb_lights, pcb_lights.Color(  0, 255,   0)     , 50); // Green
+  colorWipe(pcb_lights, pcb_lights.Color(  0,   0, 255)     , 50); // Blue
 }
